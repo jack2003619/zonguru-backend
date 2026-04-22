ff32ee737aad7247ec0341c00294989c4c529278
jack2003619 <nyi78317@gmail.com>
2026-04-22 16:15:39 +0630

    Initialize Express server with deposit and withdraw endpoints

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let users = {};
+
+app.post("/deposit", (req, res) => {
+  const { user, amount } = req.body;
+  users[user] = (users[user] || 0) + Number(amount);
+  res.json({ success: true, balance: users[user] });
+});
+
+app.post("/withdraw", (req, res) => {
+  const { user, amount } = req.body;
+  users[user] = (users[user] || 0) - Number(amount);
+  res.json({ success: true, balance: users[user] });
+});
+
+app.get("/", (req,res)=>{
+  res.send("Zonguru Backend Live 🚀");
+});
+
+app.listen(process.env.PORT || 3000);
