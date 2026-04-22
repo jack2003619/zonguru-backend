3e08f788e3ec5606d4c77a1cf453f54bfb1df1bd
jack2003619 <nyi78317@gmail.com>
2026-04-22 20:28:20 +0630

    Integrate MongoDB for user deposits and withdrawals

--- index.js
+++ index.js
@@ -1,40 +1,63 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-let users = {};
+// 🔥 MongoDB connect
+mongoose.connect("mongodb+srv://zonguru:<Wsadwsad123>@cluster0.jixn4t6.mongodb.net/?appName=Cluster0")
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Zonguru API Running 🚀");
+// User Schema
+const UserSchema = new mongoose.Schema({
+  username: String,
+  balance: Number
 });
 
-app.post("/deposit", (req, res) => {
-  const { user, amount } = req.body;
-  users[user] = (users[user] || 0) + Number(amount);
-  res.json({ success: true, balance: users[user] });
-});
+const User = mongoose.model("User", UserSchema);
 
-app.post("/withdraw", (req, res) => {
+// Deposit
+app.post("/deposit", async (req, res) => {
   const { user, amount } = req.body;
 
-  if ((users[user] || 0) < amount) {
-    return res.json({ success: false, message: "Not enough balance" });
+  let u = await User.findOne({ username: user });
+
+  if (!u) {
+    u = new User({ username: user, balance: 0 });
   }
 
-  users[user] -= amount;
-  res.json({ success: true, balance: users[user] });
-});
-
-app.listen(3000, () => console.log("Server running"));  if (!u || u.balance < amount) {
-    return res.json({ success: false, message: "Not enough balance" });
-  }
-
-  u.balance -= Number(amount);
+  u.balance += Number(amount);
   await u.save();
 
+  res.json({ success: true, balance: u.balance });
+});
+
+// Withdraw
+app.post("/withdraw", async (req, res) => {
+  const { user, amount } = req.body;
+
+  let u = await User.findOne({ username: user });
+
+  if (!u || u.balance < amount) {
+    return res.json({ success: false, message: "Not enough balance" });
+  }
+
+  u.balance -= amount;
+  await u.save();
+
+  res.json({ success: true, balance: u.balance });
+});
+
+// Balance check
+app.get("/balance/:user", async (req, res) => {
+  const u = await User.findOne({ username: req.params.user });
+  res.json({ balance: u ? u.balance : 0 });
+});
+
+app.listen(3000, () => console.log("Server running"));
   res.json({ success: true, balance: u.balance });
 });
 
