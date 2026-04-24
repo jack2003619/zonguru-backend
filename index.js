1b1b727756d3ba4fa2cab5db21704449131d260f
jack2003619 <nyi78317@gmail.com>
2026-04-24 16:48:32 +0630

    Refactor MongoDB connection and add Product model

--- index.js
+++ index.js
@@ -3,87 +3,48 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connect
-mongoose.connect("mongodb+srv://zonguru:Wsadwsad123@cluster0.jixn4t6.mongodb.net/zonguru")
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-// User schema
-const UserSchema = new mongoose.Schema({
+// Models
+const User = mongoose.model("User", {
   username: String,
   password: String,
-  balance: { type: Number, default: 0 }
+  balance: { type: Number, default: 0 },
+  vipLevel: { type: Number, default: 1 }
 });
 
-const User = mongoose.model("User", UserSchema);
+const Product = mongoose.model("Product", {
+  name: String,
+  image: String,
+  price: Number,
+  vipLevel: Number
+});
 
-// test route
+// TEST
 app.get("/", (req, res) => {
-  res.send("Zonguru API Running with DB");
+  res.send("API WORKING");
 });
 
-// REGISTER
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-
-  const existing = await User.findOne({ username });
-  if (existing) {
-    return res.json({ success: false, message: "User already exists" });
-  }
-
-  const user = new User({ username, password, balance: 0 });
-  await user.save();
-
-  res.json({ success: true, message: "Registered" });
+// PRODUCTS
+app.get("/products", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
 });
 
-// LOGIN
-app.post("/login", async (req, res) => {
-  const { username, password } = req.body;
-
-  const user = await User.findOne({ username, password });
-
-  if (!user) {
-    return res.json({ success: false, message: "Invalid login" });
-  }
-
-  res.json({
-    success: true,
-    user: user.username,
-    balance: user.balance
+// VIP PRODUCTS
+app.get("/products/:vip", async (req, res) => {
+  const vip = Number(req.params.vip);
+  const products = await Product.find({
+    vipLevel: { $lte: vip }
   });
+  res.json(products);
 });
 
-// DEPOSIT
-app.post("/deposit", async (req, res) => {
-  const { user, amount } = req.body;
-
-  const u = await User.findOne({ username: user });
-
-  u.balance += Number(amount);
-  await u.save();
-
-  res.json({ success: true, balance: u.balance });
-});
-
-// WITHDRAW
-app.post("/withdraw", async (req, res) => {
-  const { user, amount } = req.body;
-
-  const u = await User.findOne({ username: user });
-
-  if (u.balance < amount) {
-    return res.json({ success: false });
-  }
-
-  u.balance -= Number(amount);
-  await u.save();
-
-  res.json({ success: true, balance: u.balance });
-});
-
-app.listen(10000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
